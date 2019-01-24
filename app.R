4b1154ee2fd65dd1ee2a01a4ee0963b5d4a689b9
khllkcm <khalilqacem@gmail.com>
2019-01-24 19:54:57 +0100

    Add standalone PCA and Rename display tables

--- app.R
+++ app.R
@@ -4,14 +4,15 @@ library(argonDash)
 library(shinycssloaders)
 library(plotly)
 library(DT)
+library(FactoMineR)
+library(factoextra)
 
 setwd("~/School/Atelier/")
-source("atelier.R")
+#source("atelier.R")
 
 options(shiny.trace = F)
 
 shiny::shinyApp(
-  
   # UI ----
   ui = argonDashPage(
     title = "Sensory Data Analysis Dashboard",
@@ -68,7 +69,6 @@ shiny::shinyApp(
     # Body ----
     
     body = argonDashBody(argonTabItems(
-      
       ## Datasets ----
       argonTabItem(
         tabName = "data",
@@ -84,35 +84,61 @@ shiny::shinyApp(
           ### Hedo dataset ----
           
           argonTab(
-            tabName = "Hedo dataset",
+            tabName = "Hedonic dataset",
             active = TRUE,
             argonRow(
               argonColumn(
                 width = 3,
-                fileInput("fileHedo", "Choose CSV File",
-                          multiple = TRUE,
-                          accept = c("text/csv",
-                                     "text/comma-separated-values,text/plain",
-                                     ".csv")),
+                fileInput(
+                  "fileHedo",
+                  "Choose CSV File",
+                  multiple = TRUE,
+                  accept = c("text/csv",
+                             "text/comma-separated-values,text/plain",
+                             ".csv")
+                ),
                 checkboxInput("headerHedo", "Header", TRUE),
-                radioButtons("sepHedo", "Separator",
-                             choices = c(Comma = ",",
-                                         Semicolon = ";",
-                                         Tab = "\t"),
-                             selected = ";"),
-                radioButtons("quoteHedo", "Quote",
-                             choices = c(None = "",
-                                         "Double Quote" = '"',
-                                         "Single Quote" = "'"),
-                             selected = '"')
+                radioButtons(
+                  "sepHedo",
+                  "Separator",
+                  choices = c(
+                    Comma = ",",
+                    Semicolon = ";",
+                    Tab = "\t"
+                  ),
+                  selected = ";"
+                ),
+                radioButtons(
+                  "quoteHedo",
+                  "Quote",
+                  choices = c(
+                    None = "",
+                    "Double Quote" = '"',
+                    "Single Quote" = "'"
+                  ),
+                  selected = '"'
+                ),
+                radioButtons(
+                  "dispHedo",
+                  "Display",
+                  choices = c(Head = "head",
+                              All = "all"),
+                  selected = "head"
+                )
                 
               ),
               argonColumn(
                 center = T,
                 width = 9,
-                div(style = 'overflow-x: scroll',
-                    dataTableOutput("contentsHedo")%>% 
-                  withSpinner(color="#5e72e4", type=7, proxy.height = "400px"))
+                div(
+                  style = 'overflow-x: scroll',
+                  dataTableOutput("contentsHedo") %>%
+                    withSpinner(
+                      color = "#5e72e4",
+                      type = 7,
+                      proxy.height = "400px"
+                    )
+                )
               )
             )
           ),
@@ -124,22 +150,42 @@ shiny::shinyApp(
             argonRow(
               argonColumn(
                 width = 3,
-                fileInput("fileSenso", "Choose CSV File",
-                          multiple = TRUE,
-                          accept = c("text/csv",
-                                     "text/comma-separated-values,text/plain",
-                                     ".csv")),
+                fileInput(
+                  "fileSenso",
+                  "Choose CSV File",
+                  multiple = TRUE,
+                  accept = c("text/csv",
+                             "text/comma-separated-values,text/plain",
+                             ".csv")
+                ),
                 checkboxInput("headerSenso", "Header", TRUE),
-                radioButtons("sepSenso", "Separator",
-                             choices = c(Comma = ",",
-                                         Semicolon = ";",
-                                         Tab = "\t"),
-                             selected = ","),
-                radioButtons("quoteSenso", "Quote",
-                             choices = c(None = "",
-                                         "Double Quote" = '"',
-                                         "Single Quote" = "'"),
-                             selected = '"'),
+                radioButtons(
+                  "sepSenso",
+                  "Separator",
+                  choices = c(
+                    Comma = ",",
+                    Semicolon = ";",
+                    Tab = "\t"
+                  ),
+                  selected = ","
+                ),
+                radioButtons(
+                  "quoteSenso",
+                  "Quote",
+                  choices = c(
+                    None = "",
+                    "Double Quote" = '"',
+                    "Single Quote" = "'"
+                  ),
+                  selected = '"'
+                ),
+                radioButtons(
+                  "dispSenso",
+                  "Display",
+                  choices = c(Head = "head",
+                              All = "all"),
+                  selected = "head"
+                ),
                 uiOutput("selectSensoSession"),
                 uiOutput("selectSensoJudge"),
                 uiOutput("selectSensoProduct")
@@ -147,9 +193,15 @@ shiny::shinyApp(
               argonColumn(
                 center = T,
                 width = 9,
-                div(style = 'overflow-x: scroll',
-                    dataTableOutput("contentsSenso")%>% 
-                      withSpinner(color="#5e72e4", type=7, proxy.height = "400px"))
+                div(
+                  style = 'overflow-x: scroll',
+                  dataTableOutput("contentsSenso") %>%
+                    withSpinner(
+                      color = "#5e72e4",
+                      type = 7,
+                      proxy.height = "400px"
+                    )
+                )
               )
             )
           )
@@ -179,12 +231,17 @@ shiny::shinyApp(
               argonColumn(
                 width = 3,
                 uiOutput("selectBoxplotVar"),
-                uiOutput("selectBoxplotFactor")),
+                uiOutput("selectBoxplotFactor")
+              ),
               argonColumn(
                 center = T,
                 width = 9,
-                plotlyOutput("boxPlot", height = "100%")%>% 
-                  withSpinner(color="#5e72e4", type=7, proxy.height = "400px")
+                plotlyOutput("boxPlot", height = "100%") %>%
+                  withSpinner(
+                    color = "#5e72e4",
+                    type = 7,
+                    proxy.height = "400px"
+                  )
               )
             )
           ),
@@ -202,8 +259,12 @@ shiny::shinyApp(
               argonColumn(
                 center = T,
                 width = 9,
-                verbatimTextOutput("anova")%>% 
-                  withSpinner(color="#5e72e4", type=7, proxy.height = "400px")
+                verbatimTextOutput("anova") %>%
+                  withSpinner(
+                    color = "#5e72e4",
+                    type = 7,
+                    proxy.height = "400px"
+                  )
               )
             )
           )
@@ -244,8 +305,12 @@ shiny::shinyApp(
               argonColumn(
                 center = T,
                 width = 9,
-                plotOutput("screePlot", height = "100%")%>% 
-                  withSpinner(color="#5e72e4", type=7, proxy.height = "600px")
+                plotOutput("screePlot", height = "100%") %>%
+                  withSpinner(
+                    color = "#5e72e4",
+                    type = 7,
+                    proxy.height = "600px"
+                  )
               )
             )
           ),
@@ -280,8 +345,12 @@ shiny::shinyApp(
               argonColumn(
                 center = T,
                 width = 9,
-                plotOutput("varPlot", height = "100%") %>% 
-                  withSpinner(color="#5e72e4", type=7, proxy.height = "600px")
+                plotOutput("varPlot", height = "100%") %>%
+                  withSpinner(
+                    color = "#5e72e4",
+                    type = 7,
+                    proxy.height = "600px"
+                  )
               )
             )
           ),
@@ -307,8 +376,12 @@ shiny::shinyApp(
               argonColumn(
                 center = T,
                 width = 9,
-                plotOutput("biPlot", height = "100%")%>% 
-                  withSpinner(color="#5e72e4", type=7, proxy.height = "600px")
+                plotOutput("biPlot", height = "100%") %>%
+                  withSpinner(
+                    color = "#5e72e4",
+                    type = 7,
+                    proxy.height = "600px"
+                  )
               )
             )
           )
@@ -326,60 +399,90 @@ shiny::shinyApp(
   
   # Server ----
   server = function(input, output) {
-    
     ## Dataset Hedo ----
+    
+    df.hedoForDisplay = reactive({
+      req(input$fileHedo)
+      df <- read.csv(
+        input$fileHedo$datapath,
+        header = input$headerHedo,
+        sep = input$sepHedo,
+        quote = input$quoteHedo
+      )
+      if (input$dispHedo == "head") {
+        return(head(df))
+      }
+      else {
+        return(df)
+      }
+    })
+    
     df.hedo = reactive({
       req(input$fileHedo)
-      df <- read.csv(input$fileHedo$datapath,
-                     header = input$headerHedo,
-                     sep = input$sepHedo,
-                     quote = input$quoteHedo)
+      df <- read.csv(
+        input$fileHedo$datapath,
+        header = input$headerHedo,
+        sep = input$sepHedo,
+        quote = input$quoteHedo
+      )
       return(df)
     })
     
     ## Display Dataset Hedo ----
     output$contentsHedo <- renderDataTable({
-      df.hedo()
-    },options = list(processing = FALSE))
+      df.hedoForDisplay()
+    }, options = list(processing = FALSE))
     
     ## Dataset Senso ----
     
-    output$selectSensoSession = renderUI(selectInput(
-      inputId = "sensoSession",
-      label = "Session:",
-      choices = colnames(df.senso_initial()),
-      selected = colnames(df.senso_initial())[1]
-    ))
+    output$selectSensoSession = renderUI(
+      selectInput(
+        inputId = "sensoSession",
+        label = "Session:",
+        choices = colnames(df.sensoForDisplay()),
+        selected = colnames(df.sensoForDisplay())[1]
+      )
+    )
     
-    output$selectSensoJudge = renderUI(selectInput(
-      inputId = "sensoJudge",
-      label = "Judge:",
-      choices = colnames(df.senso_initial()),
-      selected = colnames(df.senso_initial())[2]
-    ))
+    output$selectSensoJudge = renderUI(
+      selectInput(
+        inputId = "sensoJudge",
+        label = "Judge:",
+        choices = colnames(df.sensoForDisplay()),
+        selected = colnames(df.sensoForDisplay())[2]
+      )
+    )
     
-    output$selectSensoProduct = renderUI(selectInput(
-      inputId = "sensoProduct",
-      label = "Product:",
-      choices = colnames(df.senso_initial()),
-      selected = colnames(df.senso_initial())[3]
-    ))
+    output$selectSensoProduct = renderUI(
+      selectInput(
+        inputId = "sensoProduct",
+        label = "Product:",
+        choices = colnames(df.sensoForDisplay()),
+        selected = colnames(df.sensoForDisplay())[3]
+      )
+    )
     
-    df.senso_initial = reactive({
+    df.sensoForDisplay = reactive({
       req(input$fileSenso)
-      df <- read.csv(input$fileSenso$datapath,
-                     header = input$headerSenso,
-                     sep = input$sepSenso,
-                     quote = input$quoteSenso)
-      
-      return(df)
+      df <- read.csv(
+        input$fileSenso$datapath,
+        header = input$headerSenso,
+        sep = input$sepSenso,
+        quote = input$quoteSenso
+      )
+      if (input$dispSenso == "head") {
+        return(head(df))
+      }
+      else {
+        return(df)
+      }
     })
     
     df.senso = reactive(({
       req(input$sensoSession)
       req(input$sensoJudge)
       req(input$sensoProduct)
-      df = df.senso_initial()
+      df = df.sensoForDisplay()
       df[[input$sensoSession]] = as.factor(df[[input$sensoSession]])
       df[[input$sensoJudge]] = as.factor(df[[input$sensoJudge]])
       df[[input$sensoProduct]] = as.factor(df[[input$sensoProduct]])
@@ -388,8 +491,8 @@ shiny::shinyApp(
     
     ## Display Dataset Senso ----
     output$contentsSenso <- renderDataTable({
-      df.senso_initial()
-    },options = list(processing = FALSE))
+      df.sensoForDisplay()
+    }, options = list(processing = FALSE))
     
     ## ANOVA ----
     output$selectAnovaVar = renderUI(selectInput(
@@ -404,14 +507,17 @@ shiny::shinyApp(
         label = "Factor:",
         choices = names(Filter(is.factor, df.senso())),
         multiple = TRUE
-      ))
+      )
+    )
     
     anovaFactors = reactive({
       req(input$anovaFactors)
     })
     
     output$anova = renderPrint({
-      summary(aov(as.formula(paste(input$anovaVar," ~ ",paste(anovaFactors(),collapse="*"))),data=df.senso()))
+      summary(aov(as.formula(paste(
+        input$anovaVar, " ~ ", paste(anovaFactors(), collapse = "*")
+      )), data = df.senso()))
     })
     
     ## Boxplot ----
@@ -422,13 +528,11 @@ shiny::shinyApp(
       choices = names(Filter(is.numeric, df.senso()))
     ))
     
-    output$selectBoxplotFactor = renderUI(
-      selectInput(
-        inputId = "boxplotFactor",
-        label = "Factor:",
-        choices = names(Filter(is.factor, df.senso()))
-      )
-    )
+    output$selectBoxplotFactor = renderUI(selectInput(
+      inputId = "boxplotFactor",
+      label = "Factor:",
+      choices = names(Filter(is.factor, df.senso()))
+    ))
     
     boxplotVar = reactive({
       req(input$boxplotVar)
@@ -439,17 +543,34 @@ shiny::shinyApp(
     })
     
     output$boxPlot <-  renderPlotly({
-      plot_ly(data=df.senso(),
-              x=df.senso()[[boxplotVar()]],
-              color=df.senso()[[boxplotFactor()]],
-              colors="RdYlBu",
-              type="box")
+      plot_ly(
+        data = df.senso(),
+        x = df.senso()[[boxplotVar()]],
+        color = df.senso()[[boxplotFactor()]],
+        colors = "RdYlBu",
+        type = "box"
+      )
+    })
+    
+    ## PCA ----
+    obj.pca = reactive({
+      df.X = summaryBy(
+        as.formula(paste(". ~ ", input$sensoProduct)),
+        data = df.senso()[,-which(names(df.senso()) %in% c(input$sensoSession, input$sensoJudge))],
+        FUN = c(mean),
+        na.rm = T,
+        keep.names = T
+      )
+      rownames(df.X) = df.X[, 1]
+      res.pca = PCA(df.X[,-1], scale.unit = F, graph = F)
+      return(res.pca)
     })
     
     ## Scree plot ----
     
+    
     output$screePlot <- renderPlot({
-      fviz_screeplot(obj.pca, choice = input$choice)
+      fviz_screeplot(obj.pca(), choice = input$choice)
     }, height = 600, width = 600)
     
     
@@ -468,7 +589,7 @@ shiny::shinyApp(
     
     output$varPlot =
       renderPlot({
-        fviz_pca_var(obj.pca,
+        fviz_pca_var(obj.pca(),
                      col.var = "cos2",
                      axes = c(as.numeric(input$axis_X), as.numeric(Y_axis()))) +
           scale_color_gradient2(
@@ -497,7 +618,7 @@ shiny::shinyApp(
     
     output$biPlot =    renderPlot({
       fviz_pca_biplot(
-        obj.pca,
+        obj.pca(),
         repel = T,
         alpha.var = "contrib",
         col.var = "cos2",
