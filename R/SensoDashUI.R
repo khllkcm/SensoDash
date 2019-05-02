c618ed2c77950dfa5e1dfc32015bfd9c33cb637c
Khalil Kacem <khllkcm@users.noreply.github.com>
2019-05-02 09:47:56 +0100

    Fix example check

--- R/SensoDashUI.R
+++ R/SensoDashUI.R
@@ -1,13 +1,15 @@
 #' @title Launch SensoDash application
 #'
 #' @description A User Interface for the SensoDash package. The UI is a Shiny application for consumer data segmentation.
+#' @export
 #'
 #' @examples
-#' library(SensoDash)
 #'
-#' SensoDashUI()
+#' if (interactive()) {
 #'
-#' @export
+#'  SensoDashUI()
+#'
+#' }
 SensoDashUI <- function() {
   shiny::runApp(system.file("app/", package = "SensoDash"), launch.browser = T)
 }
